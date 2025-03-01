67e02571210fb60dfa80485622922c13a8c33e78
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-01 15:55:25 +0530

    [Shivam Kumar Jha] Add . Added the user frendly error response in case the validation fails.

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 public class EmployeeDTO {
 	private Long id;
+	@NotBlank(message = "Name can't remain blank")
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Invalid name entered.")
     private String name;
     private String gender;
