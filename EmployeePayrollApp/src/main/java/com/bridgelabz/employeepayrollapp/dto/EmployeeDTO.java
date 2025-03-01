2718ebf4915baeeb5ba68837618e66ede751322d
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-01 12:41:30 +0530

    [Shivam Kumar Jha] Add . Added Getters and Setters in DTO using Lambok library

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+// Adding the Getter and Setter using annotation from lambok library
 @Getter
 @Setter
 public class EmployeeDTO {
